56b7a623ce46846534e7d6fb559f8313c751bfb0
ArjenForiers <Arjen.Foriers@student.pxl.be>
2018-05-28 12:11:39 +0200

    fdqf

---
+++ WKPloegen.js
@@ -0,0 +1,14 @@
+function voegLandToe() {
+    let land = {
+        name: document.getElementById("naam").value,
+        shortcode: document.getElementById("shortcode").value,
+        beschrijving: document.getElementById("beschrijving").value
+    };
+
+    document.createElement("DIV");
+    document.body.appendChild()
+
+}
+
+
+
