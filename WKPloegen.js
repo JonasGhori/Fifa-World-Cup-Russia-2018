1c6405a04127c3e7f886c47dfc0463a93dc0e668
destan <yusufdestan10@hotmail.com>
2018-05-29 17:16:41 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	index.html

--- WKPloegen.js
+++ WKPloegen.js
@@ -1,14 +1,55 @@
-function voegLandToe() {
-    let land = {
-        name: document.getElementById("naam").value,
-        shortcode: document.getElementById("shortcode").value,
-        beschrijving: document.getElementById("beschrijving").value
-    };
+let naamIngave = document.getElementById('naam');
+let shortcode= document.getElementById('shortcode');
+let beschrijving = document.getElementById('beschrijving');
+let button = document.getElementById('button');
 
-    document.createElement("DIV");
-    document.body.appendChild()
+let landen = [];
+
+button.addEventListener('click', function () {
+    let land={name: naamIngave.value,
+         code:shortcode.value,
+        besch:beschrijving.value };
+
+    landen.push(land);
+    localStorage.setItem("opgeslagenLanden",JSON.stringify(landen));
+    laadLanden();
+})
+
+function laadLanden() {
+    document.getElementById("landenLijstBox").innerHTML="";
+
+    for (let i=0; i<landen.length; i++){
+        let vlagLand = document.createElement('img');
+        vlagLand.src="resources/landen/"+landen[i].code +".png";
+
+        let ploegbox= document.createElement('div');
+        if (i%3 ===0){
+            ploegbox.setAttribute("class","cold");
+        }else{
+            ploegbox.setAttribute("class","green");
+        }
+
+
+        let titel = document.createElement('h2');
+        titel.innerHTML =  landen[i].code.substring(0,1).toUpperCase() || landen[i].code.substring(1, landen[i].code.length).toLowerCase()
+
+        let desc = document.createElement('p');
+        desc.id = "beschrijvingOutput";
+        desc.innerHTML = landen[i].besch;
+        ploegbox.append(vlagLand);
+        ploegbox.append(titel);
+        ploegbox.append(desc);
+        document.getElementById("landenLijstBox").appendChild(ploegbox)
+
+
+    }
 
 }
 
-
+landen = JSON.parse(localStorage.getItem("opgeslagenLanden")) || [];
+laadLanden();
+document.getElementById("refresh").addEventListener('click',function () {
+    landen=[];
+    laadLanden();
+})
 
